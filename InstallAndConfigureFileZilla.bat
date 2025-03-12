0fe0e1ffb78ca5ac12e1cf7c6c8f64d3cc0f9736
Doğukan Eren <46019536+dogukaneren@users.noreply.github.com>
2025-03-12 10:22:38 +0300

    Create InstallAndConfigureFileZilla.bat

---
+++ InstallAndConfigureFileZilla.bat
@@ -0,0 +1,41 @@
+@echo off
+rem author Doğukan EREN
+
+echo Slient install starting...
+
+FileZilla_Server.exe /S /user=all
+
+echo Kurulum tamamlandı!
+
+set "FZ_INSTALL_DIR=C:\Program Files (x86)\FileZilla Server"
+
+set "TARGET_CONFIG=%FZ_INSTALL_DIR%\FileZilla Server.xml"
+echo %TARGET_CONFIG%
+set "CONFIG_FILE=FileZilla Server.xml"
+
+if exist "%CONFIG_FILE%" (
+    echo Config found, copying...
+
+    rem Backup old config
+    if exist "%TARGET_CONFIG%" (
+        echo Eski config dosyası bulundu, old.xml olarak yedekleniyor...
+        rename "%TARGET_CONFIG%" "old.xml"
+    )
+
+    timeout /t 2 /nobreak > nul
+    
+    taskkill.exe /F /IM "filezilla*"
+    copy /Y "%CONFIG_FILE%" "%FZ_INSTALL_DIR%\FileZilla Server.xml"
+    
+    timeout /t 3 /nobreak > nul
+
+    echo "Services staring..."
+    sc start "FileZilla Server"
+
+    start "" "%FZ_INSTALL_DIR%\FileZilla Server Interface.exe"
+    
+) else (
+    echo No additional config found just installed please configure manually!!
+    sleep 5
+)
+exit
